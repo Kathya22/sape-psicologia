d1c9fb4d6f1771e6ec3f9534b97e3999cd0878a8
Kathya22 <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>
2026-03-20 01:26:29 -0400

    Actualización del index: correciones de diseño

---
+++ enviar.php
@@ -0,0 +1,26 @@
+<?php
+
+$destinatario = "informes@sapepsicologos.com"; // 👈 correo del cliente
+
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$tipo = $_POST['tipo_atencion'];
+$modalidad = $_POST['modalidad'];
+$mensaje = $_POST['mensaje'];
+
+$contenido = "Nombre: $nombre\n";
+$contenido .= "Teléfono: $telefono\n";
+$contenido .= "Correo: $correo\n";
+$contenido .= "Tipo de atención: $tipo\n";
+$contenido .= "Modalidad: $modalidad\n";
+$contenido .= "Mensaje: $mensaje\n";
+
+$headers = "From: $correo";
+
+mail($destinatario, "Nueva solicitud desde la web", $contenido, $headers);
+
+// Redireccionar después de enviar
+header("Location: gracias.html");
+
+?>
